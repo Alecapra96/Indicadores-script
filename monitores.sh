cccae07d87a2592fa56d3eb3a10be61456e9849c
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-10-26 15:15:30 -0300

    Creo el script

---
+++ monitores.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+echo "Bienvenidos al script para crear los monitores"
+echo "_______________________________________________"
+echo "Cargando..."
+wait 2000
+echo "Iniciando"
+echo "Actualizo el sistema"
+sudo apt -y install && sudo apt -y upgrade
+
+echo "Instalo dependencias bases"
+sudo apt -y install --no-install-suggests --no-install-recommends xserver-xorg-core software-properties-common broadcom-sta-dkms cmake libfreetype6-dev libfontconfig1-dev xclip build-essential libx11-dev libxft-dev xterm build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev 
+echo "Instalo BSPWM"
+git clone https://github.com/baskerville/bspwm.git
+git clone https://github.com/baskerville/sxhkd.git
+cd bspwm/
+make
+sudo make install
+cd ../sxhkd/
+make
+sudo make install
+sudo apt install bspwm
+echo "Creo Los archivos de configuracion"
+mkdir ~/.config
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+cd ../bspwm/
+cp examples/bspwmrc ~/.config/bspwm/
+chmod +x ~/.config/bspwm/bspwmrc
+cp examples/sxhkdrc ~/.config/sxhkd/
+
+echo "Instalo el script de startx"
+sudo apt -y install xinit
+echo "Creo el archivo .xinitrc"
+sudo mv ~/Monitores-script/.xinitrc ~/.xinitrc
+
+
+
+echo "Instalo programas base" 
+sudo apt -y install  rdesktop slim 
+
+echo "Remuevo cosas que ocupan espacio"
+sudo apt -y remove snapd
+sudo apt -y purge snapd 
+
+
+echo "Instalo google chrome"
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt -y update
+sudo apt -y install google-chrome-stable
+
+echo "Hago el autologin"
+sudo rm /etc/slim.conf
+sudo mv ~/Monitores-script/slim.conf /etc/
+
+echo "Haciendo ajustes finales..."
+wait 2000
+echo "Script ejectuado.."
+echo "Echo por alejandro Capra."
+
