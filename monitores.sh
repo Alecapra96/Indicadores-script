f4ff102901e2bdf3af3c5223760d8672f81766b8
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-11-15 16:44:30 -0300

    Update monitores.sh

--- monitores.sh
+++ monitores.sh
@@ -4,7 +4,7 @@ sudo apt -y install dialog
 fundialog=${fundialog=dialog}
 DIALOG=${DIALOG=dialog}
 tempfile=`tempfile 2>/dev/null` || tempfile=/tmp/test$$
-trap "rm -f $tempfile" 0 1 2 5 1
+trap "rm -f $tempfile" 0 1 2 5 15
 
 
 #_______________
